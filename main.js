15ae11ca13ca65a24861d84a4815b5d0e96776de
Imran-Da-pro <124009163+Imran-Da-pro@users.noreply.github.com>
2023-07-20 19:45:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+https://teachablemachine.withgoogle.com/models/xNwrS2Nuu/model.json/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+ camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+ 
+ console.log('ml5 version', ml5.version);
+
+ classifier = mlf.imageClassifier('https://teachablemachine.withgoogle.com/models/xNwrS2Nuu/model.json',modelLoaded);
